7716b3697999491f104b8924944a0e3cfefcbc78
Dhruval Anandkar <155497084+DhruvalAnandkar@users.noreply.github.com>
2025-10-06 19:22:06 -0400

    Update vite.config.js

--- pitch_competition/article-bias-checker/frontend/vite.config.js
+++ pitch_competition/article-bias-checker/frontend/vite.config.js
@@ -1,12 +1,7 @@
-// frontend/vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:4000'
-    }
-  }
+  base: './', // <-- important for static hosting
 });
